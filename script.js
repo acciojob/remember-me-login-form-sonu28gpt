01578b80b8df63b09d038a7ecfc97733a590d5c0
sonu28gpt <98702109+sonu28gpt@users.noreply.github.com>
2025-01-21 13:24:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
 //your JS code here. If required.
+let existBtn=document.querySelector('#existing');
+function exist(){
+        let userDetail=(localStorage.getItem('user'))?localStorage.getItem('user'):'{}';
+        // console.log(typeof userDetail);
+        userDetail=JSON.parse(userDetail)
+        console.log(userDetail,Object.keys(userDetail))   
+        if(Object.keys(userDetail).length){
+         
+        	document.querySelector('#existing').style.display='block';
+        }else{
+			existBtn.style.display='none';
+		}
+}
+existBtn.addEventListener('click',()=>{
+  alert("Logged in as .");
+})
+
+let form=document.querySelector('form');
+
+
+form.addEventListener('submit',(e)=>{
+  e.preventDefault();
+  let data=new FormData(form);
+  let dataObj=Object.fromEntries(data);
+  console.log(dataObj);
+    if(dataObj?.check){
+      console.log('hi');
+      localStorage.setItem('user',JSON.stringify(dataObj));
+
+    }else{
+      localStorage.clear();
+      // existBtn.style.display='none';
+    }
+    alert('Logged in as .');
+    exist();
+    form.reset();
+})
+   
+exist();
